9d801c5cd4460ccc335093e3a88943726830e7b1
dawidsappich <dawidsappich@gmail.com>
2017-11-06 21:17:20 +0100

    subscribe to get notified about changes in user status

--- src/app/containers/app/app.component.ts
+++ src/app/containers/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AuthService } from "../../../auth/service/auth.service";
+import { Store } from 'store';
+
+// interface
+import { Member } from "../models/user";
+
+// rxjs
+import { Observable } from "rxjs/Observable";
+import { Subscription } from "rxjs/Subscription";
 
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   templateUrl: 'app.component.html'
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+
+  user$: Observable<any>;
+  subscription: Subscription;
+
+  constructor(
+    private store: Store,
+    private authService: AuthService
+  ) { }
+
+  ngOnInit() {
+    // get notified when user status changes
+    this.subscription = this.authService.auth$.subscribe();
+    this.user$ = this.store.select<Member>('user');
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
