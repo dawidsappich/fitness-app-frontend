6bbf0c298e61e86fb6f82a5f65ca6ed4b93ea4e7
dawidsappich <dawidsappich@gmail.com>
2017-11-08 21:04:21 +0100

    add routes for child modules

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
 
-import { MealsModule } from "./meals/meals.module";
-
 import { AuthGuard } from "../auth/shared/guards/auth.guard";
 
 export const ROUTES: Routes = [
 	// lazy load module
-	{ path: 'schedule', canDeactivate: [AuthGuard], loadChildren: './schedules/schedules.module#SchedulesModule' },
-	{ path: 'meals', canDeactivate: [AuthGuard], loadChildren: './meals/meals.module#MealsModule' },
-	{ path: 'workouts', canDeactivate: [AuthGuard], loadChildren: './workouts/workouts.module#WorkoutsModule' },
+	{ path: 'schedule', loadChildren: './schedules/schedules.module#SchedulesModule' },
+	{ path: 'meals', loadChildren: './meals/meals.module#MealsModule' },
+	{ path: 'workouts', loadChildren: './workouts/workouts.module#WorkoutsModule' },
 ]
 
 @NgModule({
