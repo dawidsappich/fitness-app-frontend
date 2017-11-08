01856fd68607a3bfdf934567e01d26aa1de1a6b8
dawidsappich <dawidsappich@gmail.com>
2017-11-08 20:19:34 +0100

    add child modules for lazy loading

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from "@angular/router";
 
 import { MealsModule } from "./meals/meals.module";
 
+import { AuthGuard } from "../auth/shared/guards/auth.guard";
+
 export const ROUTES: Routes = [
 	// lazy load module
-	{ path: 'schedule', loadChildren: './schedules/schedules.module#SchedulesModule' },
-	{ path: 'meals', loadChildren: './meals/meals.module#MealsModule' },
-	{ path: 'workouts', loadChildren: './workouts/workouts.module#WorkoutsModule' },
+	{ path: 'schedule', canDeactivate: [AuthGuard], loadChildren: './schedules/schedules.module#SchedulesModule' },
+	{ path: 'meals', canDeactivate: [AuthGuard], loadChildren: './meals/meals.module#MealsModule' },
+	{ path: 'workouts', canDeactivate: [AuthGuard], loadChildren: './workouts/workouts.module#WorkoutsModule' },
 ]
 
 @NgModule({
