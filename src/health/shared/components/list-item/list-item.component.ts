43c7c94b91c570f4b2f06ef9dc6bfdfdf016bb19
dawidsappich <dawidsappich@gmail.com>
2017-11-12 19:35:32 +0100

    add list-item as sheard component to display all kind of items

---
+++ src/health/shared/components/list-item/list-item.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, Output, ChangeDetectionStrategy, EventEmitter } from '@angular/core';
+
+@Component({
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	selector: 'list-item',
+	styleUrls: ['list-item.component.scss'],
+	templateUrl: 'list-item.component.html'
+})
+export class ListItemComponent {
+
+	isToggled = false;
+
+	@Input()
+	item: any;
+
+	@Output()
+	remove = new EventEmitter<any>();
+
+	constructor() { }
+
+	getItemRoute(item: any) {
+		return [`../meals`, item._id];
+	}
+
+	removeItem() {
+		this.remove.emit(this.item);
+	}
+
+	toggle() {
+		this.isToggled = !this.isToggled;
+	}
+
+}
