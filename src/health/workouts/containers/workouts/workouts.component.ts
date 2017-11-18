683cae2f5a5dfc43a3b8e537a89af587a01a24c0
dawidsappich <dawidsappich@gmail.com>
2017-11-18 20:04:45 +0100

    setup workouts

--- src/health/workouts/containers/workouts/workouts.component.ts
+++ src/health/workouts/containers/workouts/workouts.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from "rxjs/Subscription";
+import { Observable } from "rxjs/Observable";
+
+import { WorkoutsService } from "../../../shared/services/workouts/workouts.service";
+import { AuthService } from "../../../../auth/service/auth.service";
+
+import { Workout } from "../../../../app/containers/models/workout";
+import { Member } from "../../../../app/containers/models/user";
 
 @Component({
 	selector: 'workouts',
 	styleUrls: ['workouts.component.scss'],
 	templateUrl: 'workouts.component.html'
 })
-export class WorkoutsComponent {
-	constructor() {}
+export class WorkoutsComponent implements OnInit {
+
+	workouts$: Observable<Workout>;
+	user: string;
+
+	constructor(
+		private workoutsService: WorkoutsService,
+		private authService: AuthService
+	) { }
+
+	ngOnInit() {
+		// get the actual user from the store to get a list of meals from the database
+		this.authService.getUser().subscribe((user: Member) => this.user = user.email);
+		this.workouts$ = this.workoutsService.getWorkouts(this.user);
+	}
+
+	removeWorkout(workoutId: any) {
+		console.log(workoutId)
+		this.workoutsService.removeWorkout(workoutId._id)
+			.subscribe(() => { this.workouts$ = this.workoutsService.getWorkouts(this.user) })
+	}
 }
